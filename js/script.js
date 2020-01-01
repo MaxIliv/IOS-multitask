a979c49ae31519c2e304713f2796d12478529e94
Max Iliv <max.r.iliv@gmail.com>
2020-01-01 20:38:48 +0200

    sjsjjs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-const yt = document.querySelector('.card--yt');
-const insta = document.querySelector('.card--instagram');
+(function() {
+  const yt = document.querySelector('.card--yt');
+  const insta = document.querySelector('.card--instagram');
 
 
-document.addEventListener('click', () => {
-  yt.classList.toggle('card--left');
-  insta.classList.toggle('card--right');
-});
+  document.addEventListener('click', () => {
+    yt.classList.toggle('card--left');
+    insta.classList.toggle('card--right');
+  });
+})();

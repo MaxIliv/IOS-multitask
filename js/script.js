0d8de8a71e691ad859a894929848a3e5c3a825d1
Max Iliv <max.r.iliv@gmail.com>
2020-01-01 20:17:15 +0200

    well, multitask ... but just a little

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const yt = document.querySelector('.card--yt');
+const insta = document.querySelector('.card--instagram');
+
+
+document.addEventListener('click', () => {
+  yt.classList.toggle('card--left');
+  insta.classList.toggle('card--right');
+});
